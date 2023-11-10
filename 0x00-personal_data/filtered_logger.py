2886b76ffaedb7c0a14c929f0149c9159e4ecccd
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-10 09:47:49 +0000

    Create project directory

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Create a function - 'filter_datum' that returns obfuscated log messages"""
+from re import sub
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    
+    Obfuscate log messages by performing regex substitution
+
+    Args:
+    a. fields - Incoming arg of a list of strs rep'ing fields to obfuscate
+    b. redaction - Incoming arg of the str to substitute with value of fields
+    c: message - Incoming arg of the string rep'ing the log input to obfuscate
+    d. separator - Incoming arg for separator character for 'message'
+
+    Returns:
+        A log message with the values in 'fields' obfuscated.
+
+    """
+    obf_log = sub(, redaction)
