0168401103b127bdcd67627401f2939f0542e524
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-11 23:01:38 +0000

    Fix pycodestyle too many lines error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,16 +10,13 @@ class Auth:
     def __init__(self):
         pass
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return False for now"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """Return None for now"""
         return None
-
 
     def current_user(Self, request=None) -> TypeVar('User'):
         """Return None for now"""
