e85f4700b2840b71e9df99936be1152b54b8e71f
Koyejo <adinlewakoyejo@yahoo.com>
2023-11-11 06:20:22 +0000

    Add function to hash & salt a password string

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Implement password encryption"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash and salt a password.
+
+    Arg:
+        password (str): incoming argument for the password str to be hashed.
+
+    Returns:
+        A salted, hashed password as a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
